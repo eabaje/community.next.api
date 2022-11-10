c1957b60975fa1c64a44bc793344c3d9f1bb982f
Ebi Abaje <eabaje@outlook.com>
2022-11-10 17:40:40 +0100

    commit@11-10-2022-3

--- routes/group.routes.js
+++ routes/group.routes.js
@@ -80,4 +80,110 @@ module.exports = function (app) {
     );
     next();
   });
+
+  // Relation
+  app.post("/api/user/addRelation", controller.addRelation);
+
+  app.put("/api/user/updateRelation/:relationId", controller.updateRelation);
+
+  app.get(
+    "/api/user/getRelation/:relationType/:relationId",
+    controller.getRelation
+  );
+
+  app.get(
+    "/api/user/getAllRelation/:relationType/:userId",
+    controller.getAllRelation
+  );
+
+  app.delete("/api/user/deleteRelation/:relationId", controller.deleteRelation);
+
+  // ChildOrSibling
+
+  app.post("/api/user/addChildOrSibling", controller.addChildOrSibling);
+
+  app.post("/api/user/updateChildOrSibling", controller.updateChildOrSibling);
+  app.get(
+    "/api/user/getChildOrSibling/:relationId",
+    controller.getChildorSibling
+  );
+
+  app.get(
+    "/api/user/getAllChildorSibling/:relationType/",
+    controller.getAllChildorSibling
+  );
+
+  app.delete(
+    "/api/user/deleteChildorSibling/:relationId",
+    controller.deleteChildorSibling
+  );
+
+  //SchoolworkPlace
+
+  app.post("/api/user/addSchoolplacework", controller.addSchoolPlaceWork);
+
+  app.post("/api/user/updateSchoolPlaceWork", controller.updateSchoolPlaceWork);
+
+  app.get(
+    "/api/user/getSchoolPlaceWork/:relationType/:Id",
+    controller.getSchoolPlaceWork
+  );
+
+  app.get(
+    "/api/user/getAllSchoolPlaceWork/:relationType/",
+    controller.getAllSchoolPlaceWork
+  );
+
+  app.delete(
+    "/api/user/deleteChildorSibling/:relationType/:Id",
+    controller.deleteSchoolPlaceWork
+  );
+
+  //User
+
+  app.get("/api/user/findAllUser/:userId", controller.findUser);
+
+  app.get("/api/user/findAllUser", controller.findAllUser);
+
+  app.get(
+    "/api/user/findAllUserBySearch/:name",
+    controller.findAllUserBySearch
+  );
+
+  app.get(
+    "/api/user/findAllUsersByDate/:startDate/:toDate",
+    controller.findAllUsersByDate
+  );
+
+  app.put("/api/user/updateUser/:userId", controller.updateUser);
+
+  app.post(
+    "/api/user/updateFile",
+
+    imageUploader.single("file"),
+    controller.updateFile
+  );
+  //docUploader.single('file'),
+  app.post(
+    "/api/user/uploadCompanyDoc",
+    docUploader.any(),
+    controller.uploadCompanyDoc
+  );
+  app.post("/api/user/delete", [authJwt.verifyToken], controller.delete);
+
+  app.post("/api/user/deleteAll", controller.deleteAll);
+
+  app.get("/api/user/findRoles", controller.findRoles);
+
+  app.get("/api/user/findUserRoles", controller.findUserRoles);
+
+  app.put("/api/user/updateRole/:roleId", controller.updateRole);
+
+  app.put("/api/user/updateUserRole/:userId", controller.updateUserRole);
+
+  app.delete(
+    "/api/user/deleteRole/:roleId",
+    [authJwt.verifyToken],
+    controller.deleteRole
+  );
 };
--- swagger.js
+++ swagger.js
@@ -1,23 +1,19 @@
-const swaggerAutogen = require('swagger-autogen')();
+const swaggerAutogen = require("swagger-autogen")();
 
-const outputFile = './swagger_output.json';
+const outputFile = "./swagger_output.json";
 const endpointsFiles = [
-  './routes/auth.routes.js',
-  './routes/carrier.routes.js',
-  './routes/delete.routes.js',
-  './routes/driver.routes.js',
-  './routes/order.routes.js',
-  './routes/payment.routes.js',
-  './routes/shipment.routes.js',
-  './routes/subscription.routes.js',
-  './routes/trip.routes.js',
-  './routes/upload.routes.js',
-  './routes/user.routes.js',
-  './routes/vehicle.routes.js',
+  "./routes/auth.routes.js",
+  "./routes/group.routes.js",
+  "./routes/post.routes.js",
+  "./routes/advert.routes.js",
+  // "./routes/order.routes.js",
+  // "./routes/payment.routes.js",
+  // "./routes/upload.routes.js",
+  "./routes/user.routes.js",
 ];
 
 swaggerAutogen(outputFile, endpointsFiles);
 
 swaggerAutogen(outputFile, endpointsFiles).then(() => {
-  require('./server.js');
+  require("./server.js");
 });
